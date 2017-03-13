6ed9c02f3568986991ab5f27351a5cbd5b2ee1b5
Charles Peden and Jennifer Beem <charles@>
2017-03-13 14:36:04 -0700

    add weather API

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,22 +1,3 @@
-var Journal = require('./../js/journal.js').journal;
-
-$(document).ready(function() {
-  $('#journal-form').submit(function(event) {
-    event.preventDefault();
-    var title = $('#title').val();
-    var body = $('#body').val();
-    var newEntry = new Journal(title, body);
-    var output = newEntry.entry();
-    output.forEach(function(element) {
-      $('#printed_entry').append("<li>" + element + "</li>");
-    });
-  });
-});
-
-$(document).ready(function(){
-  $('#time').text(moment());
-});
-
 var apiKey = "c8b1d7b11ba4cd220acb5be6d1da8397";
 
 $(document).ready(function() {
